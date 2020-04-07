3cc53880b8ead1be7da9098bccf5e0091224fd53
Ed Lewis <edlewis89@msn.com>
2020-04-07 09:11:19 -0400

    Added show action to articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+
+  resources :articles, only: [:show,:edit, :index, :new]
 end
