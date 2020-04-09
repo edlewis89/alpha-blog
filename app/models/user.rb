04ebdd46e6569b4b46c02483e62e203dfc250dca
Ed Lewis <edlewis89@msn.com>
2020-04-09 15:05:43 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
+require_dependency '../validators/email_validator.rb'
 class User < ApplicationRecord
 
-  validates :username, presence: true
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  validates :email, :presence => true,
+            :uniqueness => true,
+            length: { maximum: 105 },
+            email: true
   has_many :articles
 
 end
